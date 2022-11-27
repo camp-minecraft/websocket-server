ec2ae2eb3f84fd08046d4e417932a444ffe0f5cc
Kaede Oishi <oishi_kaede@ca-techkids.com>
2022-11-28 06:35:12 +0900

    wip

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const uuid = require("uuid");
 const fetch = require("node-fetch");
 const { response } = require("express");
 const { create } = require("domain");
+const { w3cwebsocket } = require("websocket");
 
 const GAS_API_URL =
     "https://script.google.com/macros/s/AKfycbyAgeH65cJV2_wZKjCUmu-UAVNedl_3Buk08kFsl4zS_v0jhKLJPUOGN-_ntn3OHNYdsA/exec";
@@ -17,8 +18,60 @@ const wss = new WebSocket.Server({
 console.info(`WebSocket is Ready. Type "/connect localhost:9999"`);
 
 // マイクラから接続された際の処理
-wss.on("connection", (ws) => {
-    console.log("Connected");
+wss.on("connection", (ws, req) => {
+
+    // 初回接続時、ID・クライアントの種類を登録
+    // HTTPリクエストのヘッダー情報から分類
+    if (req.headers["user-agent"] === undefined) {
+        ws.client = "Minecraft";
+        ws.firstConnectedTime = new Date().getTime();
+        let prevMinecraftWebsocketId = ws.minecraftWebsocketId;
+        ws.minecraftWebsocketId = uuid.v4();
+        console.log(`[MinecraftWebsocketID Change Log] ${prevMinecraftWebsocketId} -> ${ws.minecraftWebsocketId}`);
+        console.log("MC wsID:" + ws.minecraftWebsocketId)
+    } else {
+        ws.client = "Browser";
+    }
+
+    // 初回接続時、全ブラウザにマイクラのクライアントリストを送信する
+    // ブラウザ側でそれを受け取り、「グループ登録」処理を開始する
+    let minecraftWebsocketList = {
+        header: {
+            messagePurpose: "submitMinecraftClientList"
+        },
+        body: {
+            clientList: []
+        }
+    }
+
+    // 全てのWS接続のうち、マイクラとのWS接続情報を登録
+    wss.clients.forEach((client) => {
+        if (client.client === "Minecraft") {
+            console.log("MC wsID:" + client.minecraftWebsocketId)
+            minecraftWebsocketList.body.clientList.push({
+                minecraftWebsocketId: client.minecraftWebsocketId,
+                group: client.groupIndex,
+                connectedTime: client.firstConnectedTime,
+                player: "",
+            })
+            // この情報を送信
+            console.log("送信する情報----------------------")
+            console.log(minecraftWebsocketList)
+            console.log("--------------------------------")
+        }
+    })
+
+    // 上記の情報を全てのブラウザに送信
+    wss.clients.forEach((client) => {
+        if (client.client === "Minecraft") return;
+        client.send(JSON.stringify(minecraftWebsocketList))
+    })
+
+    // マイクラクライアントのプレイヤー情報を取得するためのコマンドを送信
+    // コマンドのrequestIDをWSに紐付け
+    // このコマンドのレスポンスが返り次第、requestIDをもとにプレイヤー情報とWSを紐付け
+    linkWebSocketIdAndGroup();
+
     // イベント購読用のJSONを組み立てる
     const subscribeMessageJSON = {
         header: {
@@ -67,8 +120,8 @@ wss.on("connection", (ws) => {
             eventName: "PlayerTeleported",
         },
     };
-    // イベント購読用のJSONをシリアライズ（文字列化）して送信
-    ws.send(JSON.stringify(subscribeMessageJSON));
+    // イベント購読用のJSONをシリアライズして送信
+    if (ws.client == "Minecraft") ws.send(JSON.stringify(subscribeMessageJSON));
     // ws.send(JSON.stringify(subscribeBlockJSON));
     // ws.send(JSON.stringify(subscribeAgentJSON));
     // ws.send(JSON.stringify(subscribeTpJSON));
@@ -76,7 +129,7 @@ wss.on("connection", (ws) => {
     // マイクラからメッセージが届いた際の処理を定義
     ws.on("message", (rawData) => {
         const data = JSON.parse(rawData); // 生メッセージをオブジェクトに変換
-        console.log(data);
+        if (data.body.type != "title") console.log(data);
         if (data.header.eventName == "PlayerMessage") {
             const msg = data.body.message;
             try {
@@ -87,8 +140,6 @@ wss.on("connection", (ws) => {
                         _msgJson.push(segment.text);
                     });
                     let _ = _msgJson.join("")
-                    console.log(_, typeof(_));
-                    console.log(JSON.parse(_));
                     msgJson = JSON.parse(_msgJson.join(""));
                 }
                 console.log("msgJson: ", msgJson);
@@ -112,11 +163,98 @@ wss.on("connection", (ws) => {
                 }
             } catch (error) {
                 if (msg.charAt(0) == "?") {
-                    sendCommand(msg, ws);
+                    sendCommand(msg);
                 }
             }
         }
-        if (!data.body.eventName) {
+        else if (!!data.header.messagePurpose) {
+            switch (data.header.messagePurpose) {
+                case "GAS Request":
+                    console.log(data);
+                    sendCommand("/" + data.body.command, data.header.commandSetId);
+
+                    break;
+
+                case "commandResponse":
+                    wss.clients.forEach((client) => {
+                        if (client.client == "Minecraft") return;
+                        client.send(JSON.stringify(data));
+                    })
+
+                    // もし/listdに対するレスポンスだったら
+                    if (data.body.maxPlayerCount) {
+                        try {
+
+                            [...wss.clients].filter((target) => {
+                                console.log(`${target.listdRequestId}(${target.client}) | ${data.header.requestId}`);
+                                return target.listdRequestId === data.header.requestId
+                            })[0].minecraftWebsocketId;
+                        } catch(e) {
+                            console.log(e);
+                        }
+
+                        wss.clients.forEach((client) => {
+                            if (client.client == "Minecraft") return;
+
+                            let sendMinecraftWebsocketId = [...wss.clients].filter((target) => {
+                                return target.listdRequestId === data.header.requestId
+                            })[0].minecraftWebsocketId;
+
+                            let sendMinecraftGroupIndex = [...wss.clients].filter((target) => {
+                                return target.listdRequestId === data.header.requestId
+                            })[0].groupIndex;
+
+                            let sendPlayersName = [];
+                            JSON.parse(data.body.details.split(" ")[1]).result.forEach((result) => {
+                                sendPlayersName.push(result.name);
+                            })
+
+                            let sendData = {
+                                header: {
+                                    messagePurpose: "submitPlayerList"
+                                },
+                                body: {
+                                    minecraftWebsocketId: sendMinecraftWebsocketId,
+                                    players: sendPlayersName,
+                                    groupIndex: sendMinecraftGroupIndex,
+                                }
+                            }
+                            console.log("プレーヤー名---------------------")
+                            console.log(sendData);
+                            console.log("-------------------------------")
+                            client.send(JSON.stringify(sendData));
+                        })
+                    }
+
+                    break;
+
+                case "submitGroup":
+                    /*送られてくるデータ
+                    {
+                        header: {
+                            messagePurpose: "submitGroup",
+                        },
+                        body: {
+                            groupIndex: groupIndex,
+                            minecraftWebsocketIdList: minecraftWebsocketIdList,
+                        }
+                    }
+                    */
+
+                    // 送られてきたデータにマイクラクライアントが含まれていたら、groupIndexをパラメータとして付与する
+                    wss.clients.forEach((client) => {
+                        if (data.body.minecraftWebsocketIdList.includes(client.minecraftWebsocketId)) {
+                            client.groupIndex = data.body.groupIndex;
+                        }
+                    })
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        else if (!data.body.eventName) {
             // メッセージにイベント名が含まれていない場合は処理を抜ける
             return;
         }
@@ -125,6 +263,7 @@ wss.on("connection", (ws) => {
             return; // WebSocketがOPEN状態でない場合は処理を抜ける
         }
     });
+
     function initWorld(group) {
         const headers = { "Accept-Encoding": "identity" };
         console.log(GAS_API_URL + `?type=init&group=${group}`);
@@ -140,16 +279,55 @@ wss.on("connection", (ws) => {
             });
     }
 
-    function sendCommand(msg) {
+    function sendCommand(msg, commandSetId = "", clientGroupList = "ABCDEFGHIJKLMNOPQRSTU".split("")) {
         let sendCmd;
         if (msg.charAt(0) == "?") {
             sendCmd = "/" + msg.split("?")[1];
         } else {
             sendCmd = msg;
         }
-        console.log(sendCmd);
-        ws.send(JSON.stringify(generateCommandRequest(sendCmd)));
+        if (sendCmd === "/listd") {
+            linkWebSocketIdAndGroup();
+            return
+        }
+        // clientList = generateClientList(clientGroupList)
+        clientList = wss.clients
+
+        clientList.forEach((client) => {
+            let _sendData = generateCommandRequest(sendCmd);
+            _sendData.header.commandSetId = commandSetId;
+            let sendData = JSON.stringify(_sendData);
+            if (client.client == "Minecraft") {
+                client.send(sendData);
+                ws.send(JSON.stringify({
+
+                    header: {
+                        messagePurpose: "linkCommandSet",
+                    },
+                    body: {
+                        commandId: JSON.parse(sendData).header.requestId,
+                        commandSetId: commandSetId,
+                        minecraftWebsocketId: client.minecraftWebsocketId,
+                        groupIndex: client.groupIndex,
+                    }
+                }));
+            }
+        })
     }
+
+    function linkWebSocketIdAndGroup() {
+        let sendCmd = "/listd"
+
+        wss.clients.forEach((client) => {
+            let sendData = generateCommandRequest(sendCmd);
+
+            if (client.client == "Minecraft") {
+                client.send(JSON.stringify(sendData));
+                client.listdRequestId = sendData.header.requestId
+            }
+        })
+    }
+
 
     function sendInitWorld(groupMembers) {
         //スコアボードのリセット
@@ -169,9 +347,31 @@ wss.on("connection", (ws) => {
     }
 
     function startAgentIntro(player) {
-        sendCommand(`/execute ${player} ~ ~ ~ agent tp -1 15 51 180`);
+        sendCommand(`/execute ${player} ~ ~ ~ agent tp -1 19 66 180`);
+        sendCommand(`/codebuilder navigate ${player} false https://minecraft.makecode.com/#tutorial:56182-20805-41056-46836`)
     }
+
+    ws.on("close", function (ws) {
+        console.log(`${ws.client} is closed`);
+        console.log(ws);
+    })
 });
+
+function generateClientList(groupList) {
+    const rootGroupList = wss.clients;
+    const resultGroupList = new Array();
+    rootGroupList.forEach((client) => {
+        if (client.client != "Minecraft") return;
+        if (groupList.includes(client.groupIndex)) {
+            resultGroupList.push(client);
+        }
+    })
+    return resultGroupList;
+}
+wss.on("close", function (ws, req) {
+    console.log("ws server is closed");
+    console.log(ws)
+})
 
 
 function createMembersData(membersData) {
@@ -223,6 +423,7 @@ function generateCommandRequest(cmd) {
             requestId: uuid.v4(), // UUIDv4を生成して指定
             messageType: "commandRequest", // commandRequestを指定
             messagePurpose: "commandRequest", // commandRequestを指定
+            commandSetId: "",
         },
         body: {
             origin: {
